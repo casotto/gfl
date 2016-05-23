456beb0225842a4ebd4f6cb0b28cdf2a33966272
Mattia Casotto <Mattia.Casotto@axa-direct.com>
2016-05-23 13:12:08 +0200

    modify setup.py1

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 # C library for the GFL solver
 module1 = Extension('libgraphfl',
-                    include_dirs = ['cpp/include/'],
-                    sources = ['cpp/src/graph_fl.c', 'cpp/src/tf_dp.c'])
+                    include_dirs = ["cpp\\include\\", 'C:\\projects\\gsl\\include'],
+                    sources = ['cpp\\src\\graph_fl.c', 'cpp\\src\\tf_dp.c'])
 
 setup(
     name='pygfl',
